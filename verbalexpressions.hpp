2113b773eb9686df60a090253ed56e1331092402
Jett <whackashoe@gmail.com>
2013-07-27 10:44:53 -0500

    ugh change copyright

--- verbalexpressions.hpp
+++ verbalexpressions.hpp
@@ -4,7 +4,7 @@
  *
  * The MIT License (MIT)
  * 
- * Copyright (c) 2013 jehna
+ * Copyright (c) 2013 whackashoe
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
